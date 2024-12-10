2f8245a88ee7d787a3dd9c78aa8bcdf98391eb07
Bastienpolak <bastien.laurent25490@gmail.com>
2024-12-10 15:26:15 +0100

    fichier Chat qui peut envoyé des nouveaux messages

--- app/src/main/java/com/assembleurnational/javachat/Chat.java
+++ app/src/main/java/com/assembleurnational/javachat/Chat.java
@@ -2,6 +2,9 @@ package com.assembleurnational.javachat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -21,7 +24,10 @@ public class Chat extends AppCompatActivity {
     boolean suite = true;
     int compteur = 0;
     String[] tabmessage ;
-
+    Button envoie;
+    String User ;
+    String Ami;
+    EditText message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,10 +38,13 @@ public class Chat extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        envoie = findViewById(R.id.envoie);
+        message = findViewById(R.id.message);
         Intent intent = getIntent();
         String user = intent.hasExtra("user") ? intent.getStringExtra("user") : "";
         String ami = intent.hasExtra("ami") ? intent.getStringExtra("amis") : "";
+        User = user;
+        Ami= ami;
         //initialisation socket client
         DatagramSocket clientSocket = null;
         try {
@@ -82,6 +91,45 @@ public class Chat extends AppCompatActivity {
 
 
         }
+
+
+    }
+    private void action(){
+        envoie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Envoie();
+            }
+        });
+    }
+
+    private void Envoie() throws IOException {
+        //initialisation socket client
+        DatagramSocket clientSocket = new DatagramSocket();
+
+
+
+        // Envoie
+        String text = "envoie_message,"+User+"," + Ami + message.getText().toString() ;
+        byte[] sentBytes = text.getBytes();
+
+        InetAddress serverAddress = InetAddress.getByName("localhost");
+
+        DatagramPacket sendPacket = new DatagramPacket(sentBytes, sentBytes.length, serverAddress, 1337);
+        clientSocket.send(sendPacket);
+
+        //reception
+        byte[] receiveBytes = new byte[256];
+        DatagramPacket receivePacket = new DatagramPacket(receiveBytes, receiveBytes.length);
+        clientSocket.receive(receivePacket);
+
+        String Message = new String (receivePacket.getData(), 0, receivePacket.getLength());
+        String[] messplit = Message.split(",");
+        if (messplit[4].equals("OK")){
+            compteur +=1;
+            tabmessage[compteur] = message.getText().toString();
+        }
+
 
     }
 }
